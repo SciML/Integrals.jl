894f5e373d27b0ba46b3ac77b695b126316ff664
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-08-12 09:52:36 -0400

    Files generated by PkgTemplates

---
+++ src/Quadrature.jl
@@ -0,0 +1,5 @@
+module Quadrature
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Quadrature
+using Test
+
+@testset "Quadrature.jl" begin
+    # Write your own tests here.
+end
