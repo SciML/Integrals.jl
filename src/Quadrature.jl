00e02893931f677b56d8705dee43f69ca06d1029
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-09-09 12:32:06 -0400

    add some stuff

--- src/Quadrature.jl
+++ src/Quadrature.jl
@@ -1,5 +1,22 @@
 module Quadrature
 
-greet() = print("Hello World!")
+using DiffEqBase, Requires
+
+@require QuadGK="1fd47b50-473d-5c70-9696-f719f8f3bcdc" begin
+    struct QuadGKJL end
+    DiffEqBase.solve(prob::QuadratureProblem,::QuadGKJL)
+end
+
+@require Cubature="667455a9-e2ce-5579-9412-b964f529a492" begin
+
+end
+
+@require MonteCarloIntegration="4886b29c-78c9-11e9-0a6e-41e1f4161f7b" begin
+
+end
+
+@require Cuba="8a292aeb-7a57-582c-b821-06e4c11590b1" begin
+
+end
 
 end # module
