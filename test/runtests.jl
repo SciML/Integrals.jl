23eaa434403392cc73e7b53484352653d9f720fd
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-19 07:13:10 -0400

    Remove Requires and split to subpackages

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,14 @@ using Pkg
 using SafeTestsets
 using Test
 
+function dev_subpkg(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.develop(PackageSpec(path=subpkg_path))
+end
+
+dev_subpkg("QuadratureCuba")
+dev_subpkg("QuadratureCubature")
+
 @time @safetestset "Interface Tests" begin include("interface_tests.jl") end
 @time @safetestset "Derivative Tests" begin include("derivative_tests.jl") end
 @time @safetestset "Infinite Integral Tests" begin include("inf_integral_tests.jl") end
