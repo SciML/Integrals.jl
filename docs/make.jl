3f769ddc31ffb396da204a44c88d7f8475bff164
ArnoStrouwen <arno.strouwen@telenet.be>
2022-10-09 06:03:03 +0200

    format

--- docs/make.jl
+++ docs/make.jl
@@ -6,14 +6,14 @@ makedocs(sitename = "Integrals.jl",
          authors = "Chris Rackauckas",
          modules = [Integrals, Integrals.SciMLBase],
          clean = true, doctest = false,
-         strict=[
-            :doctest,
-            :linkcheck,
-            :parse_error,
-            :example_block,
-            # Other available options are
-            # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-        ],
+         strict = [
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
                                   canonical = "https://integrals.sciml.ai/stable/"),
