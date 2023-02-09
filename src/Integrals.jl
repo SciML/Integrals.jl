efd8fadf4f7da1aaa004e37f83b8fba55b00f6b8
ArnoStrouwen <arno.strouwen@telenet.be>
2023-02-09 17:14:48 +0100

    weak dep/require Zygote

--- src/Integrals.jl
+++ src/Integrals.jl
@@ -6,11 +6,7 @@ end
 
 using Reexport, MonteCarloIntegration, QuadGK, HCubature
 @reexport using SciMLBase
-using Zygote, LinearAlgebra
-
-import ChainRulesCore
-import ChainRulesCore: NoTangent
-import ZygoteRules
+using LinearAlgebra
 
 include("init.jl")
 include("algorithms.jl")
@@ -60,7 +56,7 @@ function checkkwargs(kwargs...)
     end
     return nothing
 end
-ChainRulesCore.@non_differentiable checkkwargs(kwargs...)
+
 function SciMLBase.solve(prob::IntegralProblem; sensealg = ReCallVJP(ZygoteVJP()),
                          do_inf_transformation = nothing, kwargs...)
     if prob.batch != 0
@@ -180,85 +176,6 @@ function __solvebp_call(prob::IntegralProblem, alg::VEGAS, sensealg, lb, ub, p;
                           maxiter = maxiters, nbins = alg.nbins, debug = alg.debug,
                           ncalls = ncalls, batch = prob.batch != 0)
     SciMLBase.build_solution(prob, alg, val, err, chi = chi, retcode = ReturnCode.Success)
-end
-
-function ChainRulesCore.rrule(::typeof(__solvebp), prob, alg, sensealg, lb, ub, p;
-                              kwargs...)
-    out = __solvebp_call(prob, alg, sensealg, lb, ub, p; kwargs...)
-    function quadrature_adjoint(Δ)
-        y = typeof(Δ) <: Array{<:Number, 0} ? Δ[1] : Δ
-        if isinplace(prob)
-            dx = zeros(prob.nout)
-            _f = x -> prob.f(dx, x, p)
-            if sensealg.vjp isa ZygoteVJP
-                dfdp = function (dx, x, p)
-                    _, back = Zygote.pullback(p) do p
-                        _dx = Zygote.Buffer(x, prob.nout, size(x, 2))
-                        prob.f(_dx, x, p)
-                        copy(_dx)
-                    end
-
-                    z = zeros(size(x, 2))
-                    for idx in 1:size(x, 2)
-                        z[1] = 1
-                        dx[:, idx] = back(z)[1]
-                        z[idx] = 0
-                    end
-                end
-            elseif sensealg.vjp isa ReverseDiffVJP
-                error("TODO")
-            end
-        else
-            _f = x -> prob.f(x, p)
-            if sensealg.vjp isa ZygoteVJP
-                if prob.batch > 0
-                    dfdp = function (x, p)
-                        _, back = Zygote.pullback(p -> prob.f(x, p), p)
-
-                        out = zeros(length(p), size(x, 2))
-                        z = zeros(size(x, 2))
-                        for idx in 1:size(x, 2)
-                            z[idx] = 1
-                            out[:, idx] = back(z)[1]
-                            z[idx] = 0
-                        end
-                        out
-                    end
-                else
-                    dfdp = function (x, p)
-                        _, back = Zygote.pullback(p -> prob.f(x, p), p)
-                        back(y)[1]
-                    end
-                end
-
-            elseif sensealg.vjp isa ReverseDiffVJP
-                error("TODO")
-            end
-        end
-
-        dp_prob = remake(prob, f = dfdp, lb = lb, ub = ub, p = p, nout = length(p))
-
-        if p isa Number
-            dp = __solvebp_call(dp_prob, alg, sensealg, lb, ub, p; kwargs...)[1]
-        else
-            dp = __solvebp_call(dp_prob, alg, sensealg, lb, ub, p; kwargs...).u
-        end
-
-        if lb isa Number
-            dlb = -_f(lb)
-            dub = _f(ub)
-            return (NoTangent(), NoTangent(), NoTangent(), NoTangent(), dlb, dub, dp)
-        else
-            return (NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(),
-                    NoTangent(), dp)
-        end
-    end
-    out, quadrature_adjoint
-end
-
-ZygoteRules.@adjoint function ZygoteRules.literal_getproperty(sol::SciMLBase.IntegralSolution,
-                                                              ::Val{:u})
-    sol.u, Δ -> (SciMLBase.build_solution(sol.prob, sol.alg, Δ, sol.resid),)
 end
 
 export QuadGKJL, HCubatureJL, VEGAS
