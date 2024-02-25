85d749c8d364aa6616fa3296b166f58de3a14d10
lxvm <lorenzo@vanmunoz.com>
2024-02-25 09:45:35 -0500

    Cubature add let bindings for type stable closures

--- ext/IntegralsCubatureExt.jl
+++ ext/IntegralsCubatureExt.jl
@@ -13,7 +13,7 @@ function Integrals.__solvebp_call(prob::IntegralProblem,
     mid = (lb + ub) / 2
 
     # we get to pick fdim or not based on the IntegralFunction and its output dimensions
-    y = if prob.f isa BatchIntegralFunction
+    prototype = if prob.f isa BatchIntegralFunction
         isinplace(prob.f) ? prob.f.integrand_prototype :
         mid isa Number ? prob.f(eltype(mid)[], p) :
         prob.f(Matrix{eltype(mid)}(undef, length(mid), 0), p)
@@ -22,21 +22,22 @@ function Integrals.__solvebp_call(prob::IntegralProblem,
         isinplace(prob.f) ? prob.f.integrand_prototype : prob.f(mid, p)
     end
 
-    @assert eltype(y)<:Real "Cubature.jl is only compatible with real-valued integrands"
+    @assert eltype(prototype)<:Real "Cubature.jl is only compatible with real-valued integrands"
 
     if prob.f isa BatchIntegralFunction
-        if y isa AbstractVector # this branch could be omitted since the following one should work similarly
+        if prototype isa AbstractVector # this branch could be omitted since the following one should work similarly
             if isinplace(prob)
                 # dx is a Vector, but we provide the integrand a vector of the same type as
                 # y, which needs to be resized since the number of batch points changes.
-                dy = similar(y)
-                f = (x, dx) -> begin
-                    resize!(dy, length(dx))
-                    prob.f(dy, x, p)
-                    dx .= dy
+                f = let y = similar(prototype)
+                    (u, v) -> begin
+                        resize!(y, length(v))
+                        prob.f(y, u, p)
+                        v .= y
+                    end
                 end
             else
-                f = (x, dx) -> (dx .= prob.f(x, p))
+                f = (u, v) -> (v .= prob.f(u, p))
             end
             if mid isa Number
                 if alg isa CubatureJLh
@@ -59,50 +60,52 @@ function Integrals.__solvebp_call(prob::IntegralProblem,
                         maxevals = maxiters)
                 end
             end
-        elseif y isa AbstractArray
-            bfsize = size(y)[begin:(end - 1)]
-            bfdim = prod(bfsize)
+        elseif prototype isa AbstractArray
+            fsize = size(prototype)[begin:(end - 1)]
+            fdim = prod(fsize)
             if isinplace(prob)
                 # dx is a Matrix, but to provide a buffer of the same type as y, we make
                 # would like to make views of a larger buffer, but CubatureJL doesn't set
                 # a hard limit for max_batch, so we allocate a new buffer with the needed size
-                f = (x, dx) -> begin
-                    dy = similar(y, bfsize..., size(dx, 2))
-                    prob.f(dy, x, p)
-                    dx .= reshape(dy, bfdim, size(dx, 2))
+                f = let fsize = fsize
+                    (u, v) -> begin
+                        y = similar(prototype, fsize..., size(v, 2))
+                        prob.f(y, u, p)
+                        v .= reshape(y, fdim, size(v, 2))
+                    end
                 end
             else
-                f = (x, dx) -> (dx .= reshape(prob.f(x, p), bfdim, size(dx, 2)))
+                f = (u, v) -> (v .= reshape(prob.f(u, p), fdim, size(v, 2)))
             end
             if mid isa Number
                 if alg isa CubatureJLh
-                    val_, err = Cubature.hquadrature_v(bfdim, f, lb, ub;
+                    val_, err = Cubature.hquadrature_v(fdim, f, lb, ub;
                         reltol = reltol, abstol = abstol,
                         maxevals = maxiters, error_norm = alg.error_norm)
                 else
-                    val_, err = Cubature.pquadrature_v(bfdim, f, lb, ub;
+                    val_, err = Cubature.pquadrature_v(fdim, f, lb, ub;
                         reltol = reltol, abstol = abstol,
                         maxevals = maxiters, error_norm = alg.error_norm)
                 end
             else
                 if alg isa CubatureJLh
-                    val_, err = Cubature.hcubature_v(bfdim, f, lb, ub;
+                    val_, err = Cubature.hcubature_v(fdim, f, lb, ub;
                         reltol = reltol, abstol = abstol,
                         maxevals = maxiters, error_norm = alg.error_norm)
                 else
-                    val_, err = Cubature.pcubature_v(bfdim, f, lb, ub;
+                    val_, err = Cubature.pcubature_v(fdim, f, lb, ub;
                         reltol = reltol, abstol = abstol,
                         maxevals = maxiters, error_norm = alg.error_norm)
                 end
             end
-            val = reshape(val_, bfsize...)
+            val = reshape(val_, fsize...)
         else
             error("BatchIntegralFunction integrands must be arrays for Cubature.jl")
         end
     else
-        if y isa Real
+        if prototype isa Real
             # no inplace in this case, since the integrand_prototype would be mutable
-            f = x -> prob.f(x, p)
+            f = u -> prob.f(u, p)
             if lb isa Number
                 if alg isa CubatureJLh
                     val, err = Cubature.hquadrature(f, lb, ub;
@@ -124,14 +127,15 @@ function Integrals.__solvebp_call(prob::IntegralProblem,
                         maxevals = maxiters)
                 end
             end
-        elseif y isa AbstractArray
-            fsize = size(y)
-            fdim = length(y)
+        elseif prototype isa AbstractArray
+            fsize = size(prototype)
+            fdim = length(prototype)
             if isinplace(prob)
-                dy = similar(y)
-                f = (x, v) -> (prob.f(dy, x, p); v .= vec(dy))
+                f = let y = similar(prototype)
+                    (u, v) -> (prob.f(y, u, p); v .= vec(y))
+                end
             else
-                f = (x, v) -> (v .= vec(prob.f(x, p)))
+                f = (u, v) -> (v .= vec(prob.f(u, p)))
             end
             if mid isa Number
                 if alg isa CubatureJLh
