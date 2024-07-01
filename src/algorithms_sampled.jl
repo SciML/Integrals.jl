6d09028bbd9996328a1bab751d4b1d2c1de2649b
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-30 20:58:37 -0400

    Add syntax highlighting tag to fenced code blocks

--- src/algorithms_sampled.jl
+++ src/algorithms_sampled.jl
@@ -7,7 +7,7 @@ Struct for evaluating an integral via the trapezoidal rule.
 
 Example with sampled data:
 
-```
+```julia
 using Integrals
 f = x -> x^2
 x = range(0, 1, length=20)
@@ -28,7 +28,7 @@ Simpson's composite 1/3 rule for non-equidistant grids.
 
 Example with equidistant data:
 
-```
+```julia
 using Integrals
 f = x -> x^2
 x = range(0, 1, length=20)
